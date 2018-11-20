20fb783044ef02fb47c638597df52477fd9ae1f3
Nikitosina1981 <nikita.krutov@gmail.com>
2018-11-20 13:30:55 +0500

    2

--- src/add/Solution.java
+++ src/add/Solution.java
@@ -9,39 +9,43 @@ public class Solution
     private static ArrayList<Integer> values = new ArrayList<Integer>(Arrays.asList(0, 5, 10, 0, 11, 14, 13, 4, 11, 8
             , 8, 7, 1, 4, 12, 11));
     private static Integer counter = 0;
-    private static HashMap<ArrayList<Integer>, Integer> combinationsStorage = new HashMap<ArrayList<Integer>,
-            Integer>();
+    private static HashMap<Integer, ArrayList<Integer>> combinationsStorage = new HashMap();
 
     public static void main(String[] args)
     {
-        Solution solution = new Solution();
-        solution.countUntilRepeat(values);
-        System.out.println(counter);
-       // combinationsStorage.keySet().forEach(e->{System.out.println(e.toString());});
+        while (!combinationsStorage.containsValue(values))
+        {// generate combinations until first match
+            countUntilRepeat();
+        }
+        // printing results
+        System.out.println("Total cycles: "+counter);
+        combinationsStorage.entrySet().forEach(e->{
+            if (e.getValue().equals(values)) System.out.println("Cycle length: "+(counter-e.getKey()+1));
+        });
     }
 
-    public void countUntilRepeat(ArrayList<Integer> list)
-    { // main recursion loop. Putting all combinations to storage until first match.
-        if (combinationsStorage.containsKey(list) || list.isEmpty()) return;
-        combinationsStorage.put(list, ++counter); // put current combination to storage
-        ArrayList<Integer> temp = new ArrayList<Integer>(); // birth of new combination list
-        temp.addAll(list);
+    public static void countUntilRepeat()
+    { // Putting all combinations to storage until first match.
+        ArrayList<Integer> tmp = new ArrayList<>();
+        tmp.addAll(values);
+        combinationsStorage.put(++counter,tmp); // put current combination to storage
         int maxValuePointer = 0;
         int maxValue = Integer.MIN_VALUE;
-        for (int i = 0; i < temp.size(); i++)
+        for (int i = 0; i < values.size(); i++)
         { // let's find maximum value & position
-            if (temp.get(i)>maxValue)
+            if (values.get(i)>maxValue)
             {
-                maxValue = temp.get(i);
+                maxValue = values.get(i);
                 maxValuePointer = i;
             }
         }
-        temp.set(maxValuePointer, 0); // set max value = 0
-        while (++maxValuePointer<temp.size())
-        {// add max value to the rest of values in list
-            temp.set(maxValuePointer, temp.get(maxValuePointer)+maxValue);
+        values.set(maxValuePointer, 0); // replace max value with 0 in list
+        while (maxValue>0)
+        {// spread max value according to task logic
+            if (++maxValuePointer==values.size()) maxValuePointer=0;
+            values.set(maxValuePointer, values.get(maxValuePointer)+1);
+            maxValue--;
         }
-        countUntilRepeat(temp);
     }
 
 }
